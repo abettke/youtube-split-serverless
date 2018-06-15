abb5999a277eb7001e01d9bd88835a68c5bae3b1
Andrew Bettke <abettke@atlatlsoftware.com>
2018-06-15 16:20:06 -0400

    Started porting webapp logic to FaaS architecture.

---
+++ src/youtube.service.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import ytdl from 'ytdl-core';
+
+const validateYoutubeURL = url => ytdl.validateURL(url);
+
+const fetchYoutubeAudio = url => {
+	const audioStream = ytdl(url, { filter: (format) => format.container === 'mp4' });
+	let audioData;
+
+	return new Promise((res, rej) => {
+		audioStream
+			.on('data', data => audioData += data)
+			.on('end', () => res(audioData))
+			.on('error', err => rej(err));
+	});
+	
+};
+
+export {
+	validateYoutubeURL,
+	fetchYoutubeAudio
+};
